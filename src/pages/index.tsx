786c67849d32aec4b8279de3625826ef00a38b26
natsuki-horiki <kd1343255@st.kobedenshi.ac.jp>
2025-05-15 09:59:23 +0900

    左側にtodoリスト表示はできたので、一旦コミット。記事読んで全削除あるかも

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 
-import React from "react";
+//react関連のプラグイン
+import React, { useState } from "react";
+
+//fullcalendar関連のプラグイン
 import FullCalendar from "@fullcalendar/react"; // FullCalendarをインポート
-import timeGridWeek from "@fullcalendar/timegrid"; // DayGridプラグインをインポート
+import timeGridWeek from "@fullcalendar/timegrid"; // timeGridプラグインをインポート
+import interactionPlugin, { Draggable, DropArg } from '@fullcalendar/interaction'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -16,9 +20,33 @@ const geistMono = Geist_Mono({
 });
 
 export default function Home() {
+  const [events,setEvents]=useState([
+    
+  ]);
+
   return (
-    <div>
-      <FullCalendar plugins={[timeGridWeek]} initialView="timeGridWeek" />
+    // 横並びにするためにflexを使用し、画面全体の高さに設定
+    <div className="flex h-screen">
+      {/* todoリスト部分(左) */}
+      <div className="w-1/4 p-4 border-r">
+        <h2 className="text-xl font-bold mb-4 text-center">ToDoリスト</h2>
+        {/* 中のリスト要素 */}
+        <ul className="space-y-2">
+          <label className="flex items-center space-x-2">
+            <input type="checkbox" />
+            <span>ミーティング準備</span>
+          </label>
+          <label className="flex items-center space-x-2">
+            <input type="checkbox" />
+            <span>資料提出</span>
+          </label>
+        </ul>
+      </div>
+      
+      {/* カレンダー部分（右） */}
+      <div className="w-3/4 p-4">
+        <FullCalendar plugins={[timeGridWeek,interactionPlugin]} initialView="timeGridWeek" />
+      </div>
     </div>
   );
 }
