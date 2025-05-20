aba0eaecc698f177f7ed39ce2806a011b7a10a6e
natsuki-horiki <kd1343255@st.kobedenshi.ac.jp>
2025-05-20 14:59:51 +0900

    todo追加フォーム作成。

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ import React, { useState } from "react";
 import FullCalendar from "@fullcalendar/react"; // FullCalendarをインポート
 import timeGridWeek from "@fullcalendar/timegrid"; // timeGridプラグインをインポート
 import interactionPlugin from "@fullcalendar/interaction";
+import { DateClickArg } from "@fullcalendar/interaction"; //クリック動作
 import { EventInput } from "fullcalendar/index.js";
 
 const geistSans = Geist({
@@ -21,46 +22,104 @@ const geistMono = Geist_Mono({
 });
 
 export default function Home() {
+  const [showForm, setShowForm] = useState(false); // フォームの表示状態
+  const [newEventStart, setNewEventStart] = useState(''); // クリックされた日時
+  const [newEventEnd, setNewEventEnd] = useState(''); // クリックされた日時
+  const [title, setTitle] = useState(''); // イベントのタイトル入力
+  
+
+  const handleDateClick = (info: DateClickArg) => {
+    alert(`クリックされた日付: ${info.dateStr}`);
+    setNewEventStart(info.dateStr); //dateStrをeventStartに与えている
+    setNewEventEnd(info.dateStr);  //dateStrをeventStopに与えている
+    setShowForm(true);
+  };
+
+  
   //表示イベント例
   const [events,setEvents]=useState<EventInput>([
     {
         id: '1',
         title: 'Qiita書く',
-        description: 'リンクアンドモチベーションのアドベントカレンダーを書く',
         start: '2025-05-21',
         end: '2025-05-23',
         backgroundColor: 'green',
-        borderColor: 'red',
         editable: true
     },
     {
         id: '2',
         title: 'Qiita投稿',
-        description: 'リンクアンドモチベーションのアドベントカレンダーを投稿する',
         start: '2025-05-18T10:00:00',
         end: '2025-05-18T15:00:00',
         backgroundColor: 'green',
-        borderColor: 'red',
         editable: true
     },
     {
         id: '3',
         title: '買い物',
-        description: 'リンクアンドモチベーションのアドベントカレンダーを投稿する',
         start: '2025-05-20T03:00:00',
         end: '2025-05-20T10:00:00',
-        backgroundColor: 'green',
-        borderColor: 'red',
+        backgroundColor: 'blue',
         editable: true
-    }
+    },
   ]);
 
   return (
     // 横並びにするためにflexを使用し、画面全体の高さに設定
     <div className="flex h-screen">
       {/* todoリスト部分(左) */}
-      <div className="w-1/4 p-4 border-r">
+      <div className="w-1/4 p-4 border-r relative">
         <h2 className="text-xl font-bold mb-4 text-center">ToDoリスト</h2>
+        <button className="text-white bottom-10 right-5 py-2 px-4 rounded-full bg-gray-500 cursor-pointer absolute"
+          //＋ボタンでtodo新規追加フォームを開く
+          onClick={()=>{
+            setShowForm(true);
+          }}
+        >+</button>
+
+        {/* ToDo新規追加用フォーム */}
+        {showForm &&(
+          <form className="border my-2 py-2 rounded">
+          <div className="flex flex-col items-center justify-center">
+            <p className="text-lg font-bold">addition</p>
+            <input  //タスクの名称
+              type="text"
+              className="w-4/5 border px-2 py-1 my-1.5 rounded bg-gray-100"
+              placeholder="Title"
+            />
+
+            <input  //タスクの開始時間
+              type="text"
+              value={newEventStart} // ← dateClickで設定された日時を表示
+              onChange={(s) => setNewEventStart(s.target.value)}
+              className="w-4/5 border px-2 py-1 my-1.5 rounded bg-gray-100"
+              placeholder="Start"
+            />
+
+            <input  //タスクの終了時間
+              type="text"
+              value={newEventEnd} // ← dateClickで設定された日時を表示
+              onChange={(e) => setNewEventEnd(e.target.value)}
+              className="w-4/5 border px-2 py-1 my-1.5 rounded bg-gray-100"
+              placeholder="End"
+            />
+
+            <input  //タスクの色
+              type="text"
+              className="w-4/5 border px-2 py-1 my-1.5 rounded bg-gray-100"
+              placeholder="Color"
+            />
+
+            <input
+              className="w-1/5 border px-2 py-1 my-1.5 rounded cursor-pointer bg-gray-100"
+              type="submit"
+              value="add"
+            />
+          </div>
+        </form>
+        )}
+        
+
         {/* 中のリスト要素 */}
         <ul className="space-y-2">
           {events.map((event:EventInput)=>(
@@ -79,7 +138,8 @@ export default function Home() {
         <FullCalendar
           plugins={[timeGridWeek, interactionPlugin]}
           initialView="timeGridWeek"
-          events={events}
+          events={events} //イベントの読み込み
+          dateClick={(info) => handleDateClick(info)} //日付クリックイベント
         />
       </div>
     </div>
